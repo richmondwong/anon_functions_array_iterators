0bb26026b9ef45774e0239dc3ce0acea7dea9c91
Richmond Wong <24490829+richmondwong@users.noreply.github.com>
2019-01-06 17:14:41 -0500

    Fixed indentation

--- anon_functions_array_iterators.js
+++ anon_functions_array_iterators.js
@@ -4,13 +4,10 @@ var input = [
   { x: 8, y: 15 }
 ];
 
-
 var result = input.map(function(input){
-var final = Math.sqrt(Math.pow(input["x"], 2) + Math.pow(input["y"], 2))
-return final
+  var final = Math.sqrt(Math.pow(input["x"], 2) + Math.pow(input["y"], 2))
+  return final
 })
-
-
 
 console.log(result[0] === 5);
 console.log(result[1] === 13);
